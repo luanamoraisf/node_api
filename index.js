a12d42a433ae3fa9f3f3f6ca96bdbdc003e28b4d
luanaflorencio <luanaflorenciopc@gmail.com>
2022-04-08 17:03:30 -0300

    Passando e chamando os dados em formato json

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const { response } = require('express')
 
 const app = express()
 
@@ -10,6 +11,15 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.json())
 app.use(cors())
 
+let data = [
+    {'1': {Nome: 'Alana', Idade: '23'}},
+    {'2': {Nome: 'João', Idade: '19'}}
+]
+
+app.get('/', (req, res) => {
+    return res.json(data)
+})
+
 app.listen(21262, () => {
-    console.log('Express started at https://localhost:21262')
+    console.log('Express started at http://localhost:21262')
 })
